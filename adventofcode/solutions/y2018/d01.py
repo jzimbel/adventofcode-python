d82f0dfa090a034f2a7b2e1f3da6a118d9d09bbe
Jon Zimbel <jzimbel@gmail.com>
2018-12-28 13:08:54 -0500

    Add solutions for year 2018 day 1

--- adventofcode/solutions/y2018/d01.py
+++ adventofcode/solutions/y2018/d01.py
@@ -2,8 +2,28 @@
 Solution for day 1 of the 2018 Advent of Code calendar.
 Run it with the command `python -m adventofcode run_solution -y 2018 1` from the project root.
 '''
+from functools import reduce
+from operator import add
 from adventofcode.types import Solution
 
+def solution_1(changes: list) -> int:
+  return reduce(add, changes, 0)
+
+def solution_2(changes: list) -> int:
+  seen = set([0])
+  frequency = 0
+  repeated = []
+
+  while len(repeated) == 0:
+    for change in changes:
+      newFrequency = frequency + change
+      frequency += change
+      if newFrequency in seen:
+        repeated.append(newFrequency)
+        break
+      seen.add(newFrequency)
+  return repeated[0]
+
 def run(data: str) -> Solution:
-  # not yet implemented!
-  return (None, None)
+  changes = [int(change) for change in data.split('\n')]
+  return (solution_1(changes), solution_2(changes))
