613b1b41eab2678b05600eb3c7c38145eb0deca9
Jon Zimbel <jzimbel@gmail.com>
2018-12-29 23:37:00 -0500

    Add tests and solutions for year 2018 day 3

--- adventofcode/solutions/y2018/d03.py
+++ adventofcode/solutions/y2018/d03.py
@@ -2,8 +2,73 @@
 Solution for day 3 of the 2018 Advent of Code calendar.
 Run it with the command `python -m adventofcode run_solution -y 2018 3` from the project root.
 '''
+from typing import List, NamedTuple, Set
 from adventofcode.types import Solution
 
+class Cell(NamedTuple):
+  x: int
+  y: int
+
+class Claim(NamedTuple):
+  id: int
+  xmin: int
+  ymin: int
+  xmax: int
+  ymax: int
+
+def line_to_claim(line: str) -> Claim:
+  parts = line.split(' ')
+  xmin = int(parts[2].split(',')[0])
+  ymin = int(parts[2].split(',')[1][:-1])
+  return Claim(
+    int(parts[0][1:]),
+    xmin,
+    ymin,
+    xmin + int(parts[3].split('x')[0]),
+    ymin + int(parts[3].split('x')[1])
+  )
+
+def do_claims_overlap(claim1: Claim, claim2: Claim) -> bool:
+  # https://stackoverflow.com/a/306332
+  return (
+    claim1.xmin < claim2.xmax
+    and claim1.xmax > claim2.xmin
+    and claim1.ymin < claim2.ymax
+    and claim1.ymax > claim2.ymin
+  )
+
+def get_overlapping_claim_cells(claim1: Claim, claim2: Claim) -> Set[Cell]:
+  claim1_cells = set(Cell(x, y) for x in range(claim1.xmin, claim1.xmax) for y in range(claim1.ymin, claim1.ymax))
+  claim2_cells = set(Cell(x, y) for x in range(claim2.xmin, claim2.xmax) for y in range(claim2.ymin, claim2.ymax))
+  return claim1_cells.intersection(claim2_cells)
+
+def solution_1(claims: List[Claim]) -> int:
+  overlapping_cells = set()
+  for i, claim1 in enumerate(claims):
+    print(len(overlapping_cells), 'overlapping cells found', end='\r')
+    for claim2 in claims[i+1:]:
+      if do_claims_overlap(claim1, claim2):
+        overlapping_cells.update(get_overlapping_claim_cells(claim1, claim2))
+  print()
+  return len(overlapping_cells)
+
+def solution_2(claims: List[Claim]) -> int:
+  # stores ids of claims that have already been found to overlap with another claim
+  claim_id_blacklist = set()
+  for i, claim1 in enumerate(claims):
+    if claim1.id in claim_id_blacklist:
+      print('Skipping claim', claim1.id, end='\r')
+      continue
+    print('Checking claim', claim1.id, end='\r')
+    has_overlap = False
+    for claim2 in claims[:i] + claims[i+1:]:
+      if do_claims_overlap(claim1, claim2):
+        has_overlap = True
+        claim_id_blacklist.add(claim2.id)
+    if not has_overlap:
+      print()
+      return claim1.id
+
 def run(data: str) -> Solution:
-  # not yet implemented!
-  return (None, None)
+  claims = [line_to_claim(line) for line in data.split('\n')]
+  return (solution_1(claims), solution_2(claims))
--- adventofcode/tests/y2018/test_2018_03.py
+++ adventofcode/tests/y2018/test_2018_03.py
@@ -2,8 +2,25 @@
 Test for year 2018, day 3 solution.
 Run tests from project root with `PYTHONPATH=$(pwd) py.test`.
 '''
-from adventofcode.solutions.y2018.d03 import run
+from adventofcode.solutions.y2018.d03 import line_to_claim, do_claims_overlap, get_overlapping_claim_cells, run
+
+data = '#1 @ 1,3: 4x4\n#2 @ 3,1: 4x4\n#3 @ 5,5: 2x2'
+
+def test_line_to_claim() -> None:
+  line = data.split('\n')[0]
+  assert line_to_claim(line) == (1, 1, 3, 5, 7)
+
+def test_do_claims_overlap() -> None:
+  claims = [line_to_claim(line) for line in data.split('\n')]
+  assert do_claims_overlap(claims[0], claims[1]) == True
+  assert do_claims_overlap(claims[0], claims[2]) == False
+  assert do_claims_overlap(claims[1], claims[2]) == False
+
+def test_get_overlapping_claim_cells() -> None:
+  claims = [line_to_claim(line) for line in data.split('\n')]
+  assert get_overlapping_claim_cells(claims[0], claims[1]) == {(3,3), (3,4), (4,3), (4,4)}
+  assert get_overlapping_claim_cells(claims[0], claims[2]) == set()
+  assert get_overlapping_claim_cells(claims[1], claims[2]) == set()
 
 def test_run() -> None:
-  # not yet implemented!
-  assert run('') == (None, None)
+  assert run(data) == (4, 3)
