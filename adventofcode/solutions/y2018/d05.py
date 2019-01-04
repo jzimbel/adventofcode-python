276db087e47aba3e08904febe0a504a01ce078fa
Jon Zimbel <jzimbel@gmail.com>
2019-01-03 22:12:02 -0500

    Add tests and solutions for year 2018 day 5

--- adventofcode/solutions/y2018/d05.py
+++ adventofcode/solutions/y2018/d05.py
@@ -2,8 +2,30 @@
 Solution for day 5 of the 2018 Advent of Code calendar.
 Run it with the command `python -m adventofcode run_solution -y 2018 5` from the project root.
 '''
+import re
+from string import ascii_lowercase
 from adventofcode.types import Solution
 
+def toggle_case(char: str) -> str:
+  return char.lower() if char.isupper() else char.upper()
+
+def react(polymer: str) -> str:
+  reduced = []
+  for char in polymer:
+    if len(reduced) == 0 or toggle_case(char) != reduced[-1]:
+      reduced.append(char)
+    else:
+      reduced.pop()
+  return ''.join(reduced)
+
+def solution_1(polymer: str) -> int:
+  return len(react(polymer))
+
+def solution_2(polymer: str) -> int:
+  return min(
+    len(react(re.sub(char, '', polymer, flags=re.IGNORECASE)))
+    for char in ascii_lowercase
+  )
+
 def run(data: str) -> Solution:
-  # not yet implemented!
-  return (None, None)
+  return (solution_1(data), solution_2(data))
--- adventofcode/tests/y2018/test_2018_05.py
+++ adventofcode/tests/y2018/test_2018_05.py
@@ -2,8 +2,17 @@
 Test for year 2018, day 5 solution.
 Run tests from project root with `PYTHONPATH=$(pwd) py.test`.
 '''
-from adventofcode.solutions.y2018.d05 import run
+from adventofcode.solutions.y2018.d05 import react, run, toggle_case
+
+data = 'dabAcCaCBAcCcaDA'
+
+def test_react() -> None:
+  assert react(data) == 'dabCBAcaDA'
+
+def test_toggle_case() -> None:
+  assert toggle_case('x') == 'X'
+  assert toggle_case('X') == 'x'
 
 def test_run() -> None:
   # not yet implemented!
-  assert run('') == (None, None)
+  assert run(data) == (10, 4)
