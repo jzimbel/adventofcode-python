bb7cde1dfd7c7c94ac78d5637040c4110bafa80d
Jon Zimbel <jzimbel@gmail.com>
2019-02-04 00:25:40 -0500

    Add tests and solutions for year 2018 day 6

--- adventofcode/solutions/y2018/d06.py
+++ adventofcode/solutions/y2018/d06.py
@@ -2,81 +2,119 @@
 Solution for day 6 of the 2018 Advent of Code calendar.
 Run it with the command `python -m adventofcode run_solution -y 2018 6` from the project root.
 '''
-from typing import List, NamedTuple, Union, Dict
+from typing import Any, Callable, List, NamedTuple, Union, Dict, Optional
 from adventofcode.types import Point, Solution, with_id
+from functools import wraps
+
+# TYPE DEFINITIONS
 
 IDPoint = with_id(Point)
+PointType = Union[Point, IDPoint]
+
+def newobj(method):
+  '''
+  method decorator that converts the method to one that creates a new object
+  instead of mutating the existing one
+  http://kracekumar.com/post/100897281440/fluent-interface-in-python
+  '''
+  @wraps(method)
+  # Well, newobj can be decorated with function, but we will cover the case
+  # where it decorated with method
+  def inner(self, *args, **kwargs):
+      obj = self.__class__.__new__(self.__class__)
+      obj.__dict__ = self.__dict__.copy()
+      method(obj, *args, **kwargs)
+      return obj
+  return inner
 
 class IDDistance(NamedTuple):
   id: int
   dist: int
 
-def manhattan(p1: Union[Point, IDPoint], p2: Union[Point, IDPoint]) -> int:
+def manhattan(p1: PointType, p2: PointType) -> int:
   return abs(p1.x - p2.x) + abs(p1.y - p2.y)
 
-class Voronoi:
-  def __init__(self, seeds: List[IDPoint]) -> None:
-    self.seeds = seeds
+class SeedGrid:
+  def __init__(self, seeds: List[IDPoint], padding: int = 0) -> None:
+    self.seeds: List[IDPoint] = seeds
     seeds_x = [p.x for p in seeds]
     seeds_y = [p.y for p in seeds]
-    self.x_min = min(seeds_x)
-    self.x_max = max(seeds_x)
-    self.y_min = min(seeds_y)
-    self.y_max = max(seeds_y)
-    self._get_initial_grid()
-    self._get_qualified_ids()
-    self._get_qualified_grid()
-
-  def _get_point_id(self, point):
-    distances = [
-      IDDistance(seed.id, manhattan(point, seed))
-      for seed in self.seeds
-    ]
-    min_distance = min(distances, key=lambda iddistance: iddistance.dist)
-    if len([1 for d in distances if d.dist == min_distance.dist]) == 1:
-      # point only gets a label if it's not tied for distance between 2+ seeds
-      return min_distance.id
-    else:
-      return None
-
-  def _get_initial_grid(self):
-    self.grid = [
-      IDPoint(
-        self._get_point_id(Point(x, y)),
-        x,
-        y
-      )
+    self.x_min: int = min(seeds_x) - padding
+    self.x_max: int = max(seeds_x) + padding
+    self.y_min: int = min(seeds_y) - padding
+    self.y_max: int = max(seeds_y) + padding
+    self.grid: List[PointType] = [
+      Point(x, y)
       for x in range(self.x_min, self.x_max + 1)
       for y in range(self.y_min, self.y_max + 1)
     ]
 
-  def _get_qualified_ids(self):
-    all_ids = {
-      seed.id
-      for seed in self.seeds
-    }
-    edge_ids = {
-      idpoint.id
-      for idpoint in self.grid
-      if idpoint.x in (self.x_min, self.x_max) or idpoint.y in (self.y_min, self.y_max)
-    }
-    self.qualified_ids = all_ids - edge_ids
-
-  def _get_qualified_grid(self):
+  @newobj
+  def map_grid(self, func: Callable[[Any, List[IDPoint]], Any]) -> None:
     self.grid = [
-      point._replace(id=point.id if point.id in self.qualified_ids else None)
+      func(point, self.seeds)
       for point in self.grid
     ]
 
-  def get_areas(self):
-    areas = {id: 0 for id in self.qualified_ids}
-    for point in self.grid:
-      if point.id is not None:
-        areas[point.id] += 1
-    return areas
+# FUNCTIONS FOR PART 1
 
-def solution_1(voronoi: Voronoi) -> int:
-  return max(voronoi.get_areas().values())
+def get_id_point(point: Point, seeds: List[IDPoint]) -> IDPoint:
+  print(f'Labelling points in x={point.x}    ', end='\r')
+  distances = [
+    IDDistance(seed.id, manhattan(point, seed))
+    for seed in seeds
+  ]
+  min_distance = min(distances, key=lambda iddistance: iddistance.dist)
+  if len([d for d in distances if d.dist == min_distance.dist]) == 1:
+    # point only gets a label if it's not tied for distance between 2+ seeds
+    return IDPoint(min_distance.id, *point)
+  else:
+    return IDPoint(None, *point)
+
+def get_qualified_ids(seedGrid: SeedGrid) -> List[int]:
+  all_ids = {
+    seed.id
+    for seed in seedGrid.seeds
+  }
+  edge_ids = {
+    idpoint.id
+    for idpoint in seedGrid.grid
+    if idpoint.x in (seedGrid.x_min, seedGrid.x_max) or idpoint.y in (seedGrid.y_min, seedGrid.y_max)
+  }
+  return all_ids - edge_ids
+
+def get_areas(seedGrid: SeedGrid, qualified_ids: List[int]) -> Dict[int, int]:
+  areas = {id: 0 for id in qualified_ids}
+  for point in seedGrid.grid:
+    if point.id is not None:
+      areas[point.id] += 1
+  return areas
+
+# FUNCTIONS FOR PART 2
+
+def get_sum_distance(point: Point, seeds: List[IDPoint]) -> int:
+  print(f'Getting sum distances for points in x={point.x}    ', end='\r')
+  return sum(
+    manhattan(point, seed)
+    for seed in seeds
+  )
+
+def solution_1(seeds: List[IDPoint]) -> int:
+  seedGrid = SeedGrid(seeds).map_grid(get_id_point)
+  print()
+  print('Filtering out regions with infinite area')
+  qualified_ids = get_qualified_ids(seedGrid)
+  voronoi = seedGrid.map_grid(
+    lambda point, seeds: point if point.id in qualified_ids else point._replace(id=None)
+  )
+  return max(get_areas(voronoi, qualified_ids).values())
+
+def solution_2(seeds: List[IDPoint], max_distance: int=10000) -> int:
+  # pad the grid in each direction by m, where n seeds * m distance from each seed = max_distance
+  padding = max_distance // len(seeds)
+  distances = SeedGrid(seeds, padding).map_grid(get_sum_distance).grid
+  print()
+  return len([d for d in distances if d < max_distance])
 
 def run(data: str) -> Solution:
   seeds = [
@@ -86,5 +124,4 @@ def run(data: str) -> Solution:
     )
     for index, line in enumerate(data.split('\n'))
   ]
-  voronoi = Voronoi(seeds)
-  return (solution_1(voronoi), None)
+  return (solution_1(seeds), solution_2(seeds))
--- adventofcode/tests/y2018/test_2018_06.py
+++ adventofcode/tests/y2018/test_2018_06.py
@@ -2,8 +2,29 @@
 Test for year 2018, day 6 solution.
 Run tests from project root with `PYTHONPATH=$(pwd) py.test`.
 '''
-from adventofcode.solutions.y2018.d06 import run
+from adventofcode.solutions.y2018.d06 import solution_1, solution_2
+from adventofcode.types import Point, with_id
 
-def test_run() -> None:
-  # not yet implemented!
-  assert run('') == (None, None)
+IDPoint = with_id(Point)
+
+data = \
+'''1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9'''
+
+seeds = [
+  IDPoint(
+    index,
+    *(int(scalar) for scalar in line.split(', '))
+  )
+  for index, line in enumerate(data.split('\n'))
+]
+
+def test_solution_1() -> None:
+  assert solution_1(seeds) == 17
+
+def test_solution_2() -> None:
+  assert solution_2(seeds, max_distance=32) == 16
