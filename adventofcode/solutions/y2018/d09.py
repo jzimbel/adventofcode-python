478a1de60d086db4ec70a3d9288fc6f466039caa
Jon Zimbel <jzimbel@gmail.com>
2019-02-12 22:21:19 -0500

    Add tests and solutions for year 2018 day 9

--- adventofcode/solutions/y2018/d09.py
+++ adventofcode/solutions/y2018/d09.py
@@ -3,7 +3,24 @@ Solution for day 9 of the 2018 Advent of Code calendar.
 Run it with the command `python -m adventofcode run_solution -y 2018 9` from the project root.
 '''
 from adventofcode.types import Solution
+from collections import deque
+import re
+
+game_scenario_pattern = re.compile(r'(\d+) players; last marble is worth (\d+)')
+
+def solution(num_players: int, max_marble_value: int) -> int:
+  scores = [0] * num_players
+  circle = deque([0])
+  for marble in range(1, max_marble_value + 1):
+    if marble % 23 == 0:
+      circle.rotate(7)
+      scores[(marble - 1) % num_players] += marble + circle.pop()
+      circle.rotate(-1)
+    else:
+      circle.rotate(-1)
+      circle.append(marble)
+  return max(scores)
 
 def run(data: str) -> Solution:
-  # not yet implemented!
-  return (None, None)
+  parameters = [int(parameter) for parameter in game_scenario_pattern.match(data).groups()]
+  return (solution(*parameters), solution(parameters[0], parameters[1] * 100))
--- adventofcode/tests/y2018/test_2018_09.py
+++ adventofcode/tests/y2018/test_2018_09.py
@@ -2,8 +2,17 @@
 Test for year 2018, day 9 solution.
 Run tests from project root with `PYTHONPATH=$(pwd) py.test`.
 '''
-from adventofcode.solutions.y2018.d09 import run
+from adventofcode.solutions.y2018.d09 import solution
 
-def test_run() -> None:
-  # not yet implemented!
-  assert run('') == (None, None)
+parameters_and_expectations = [
+  ((9, 25), 32),
+  ((10, 1618), 8317),
+  ((13, 7999), 146373),
+  ((17, 1104), 2764),
+  ((21, 6111), 54718),
+  ((30, 5807), 37305)
+]
+
+def test_solution_1() -> None:
+  for parameters, expectation in parameters_and_expectations:
+    assert solution(*parameters) == expectation
